6e5ea6f0aaa81f8eb55a68b3a0749a962033c213
Yuvraj <mryuvrajsbhadauria@gmail.com>
2022-07-02 20:13:33 +0530

    SelectLocationFragment.kt UI created

---
+++ app/src/main/java/com/yuvraj/cab_booking_app/fragments/SelectLocationFragment.kt
@@ -0,0 +1,20 @@
+package com.yuvraj.cab_booking_app.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.airbnb.lottie.LottieAnimationView
+import com.yuvraj.cab_booking_app.R
+
+class SelectLocationFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_select_location, container, false)
+    }
+}
